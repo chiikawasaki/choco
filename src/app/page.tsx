12b3d8ace70893f1d532089df18874c744b33e03
AOi126 <yamaa439966@gmail.com>
2025-08-07 23:13:48 +0900

    サイドバーを無地の薄水色に

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,20 +19,7 @@ export default function Home() {
         <Portal>
           <Drawer.Backdrop />
           <Drawer.Positioner>
-            <Drawer.Content>
-              <Drawer.Header>
-                <Drawer.Title>Drawer Title</Drawer.Title>
-              </Drawer.Header>
-              <Drawer.Body>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                </p>
-              </Drawer.Body>
-              <Drawer.Footer>
-                <Button variant="outline">Cancel</Button>
-                <Button>Save</Button>
-              </Drawer.Footer>
+            <Drawer.Content style={{ backgroundColor: "#e0f7fa" }}>
               <Drawer.CloseTrigger asChild>
                 <CloseButton size="sm" />
               </Drawer.CloseTrigger>
