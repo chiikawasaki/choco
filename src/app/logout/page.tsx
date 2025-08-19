4ee5830aa00a239fe4cf09d7a0d4c5e73857f7fb
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-08-19 21:58:55 +0900

    fix: logout

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,20 +1,27 @@
-import { supabase } from "@/lib/supabase";
+"use client";
 
-const Header = () => {
+import { toaster } from "@/components/ui/toaster";
+import { supabase } from "@/lib/supabase";
+import { Button } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+
+const Logout = () => {
+  const router = useRouter();
   const handleLogout = async () => {
     const { error } = await supabase.auth.signOut();
     if (error) {
       console.error(error.message);
     } else {
       console.log("User logged out");
+      toaster.create({
+        title: "ログアウトしました",
+        type: "success",
+      });
+      router.push("/");
     }
   };
 
-  return (
-    <header>
-      <button onClick={handleLogout}>Logout</button>
-    </header>
-  );
+  return <Button onClick={handleLogout}>Logout</Button>;
 };
 
-export default Header;
+export default Logout;
