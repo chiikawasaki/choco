8fde6cda1e14ff152b52a7f27b9d72354978f1d3
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2025-08-26 18:26:40 +0900

    fix: レイアウト

--- src/app/Searchbar.tsx
+++ src/app/Searchbar.tsx
@@ -2,23 +2,25 @@ import { Box, Input, InputGroup } from "@chakra-ui/react";
 import { Search, SlidersHorizontal } from "lucide-react";
 const Searchbar = () => {
   return (
-    <InputGroup
-      endElement={
-        <Box display="flex" gap="0.5rem">
-          <Search />
-          <SlidersHorizontal />
-        </Box>
-      }
-    >
-      <Input
-        placeholder="キーワードを入力"
-        size="sm"
-        color="black"
-        css={{
-          borderRadius: "30px",
-        }}
-      />
-    </InputGroup>
+    <Box w="300px">
+      <InputGroup
+        endElement={
+          <Box display="flex" gap="0.5rem">
+            <Search />
+            <SlidersHorizontal />
+          </Box>
+        }
+      >
+        <Input
+          placeholder="キーワードを入力"
+          size="sm"
+          color="black"
+          css={{
+            borderRadius: "30px",
+          }}
+        />
+      </InputGroup>
+    </Box>
   );
 };
 export default Searchbar;
